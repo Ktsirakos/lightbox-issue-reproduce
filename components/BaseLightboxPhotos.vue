233c9c2c7068141fce2c478816fa186f7e32e3c2
Konstantinos Tsirakos <tkonstantinos1996@hotmail.com>
2020-10-16 12:13:41 +0100

    first commit

---
+++ components/BaseLightboxPhotos.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="light-box-photos-container">
+    <lightbox
+      class="mt-3"
+      :cells="cells"
+      :items="items"
+    ></lightbox>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+export default {
+  props: {
+    cells: {
+      type: Number,
+      default: 1,
+    },
+    items: {
+      type: Array,
+    },
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+.light-box-photos-container {
+  & >>> div {
+    height: 160px;
+    margin: 0;
+  }
+}
+</style>
---
+++ components/Logo.vue
@@ -0,0 +1,36 @@
+<template>
+  <base-lightbox-photos
+    :cells="1"
+    :items="images"
+  />
+</template>
+
+<script>
+import BaseLightboxPhotos from "@/components/BaseLightboxPhotos";
+export default {
+  components: {
+    BaseLightboxPhotos,
+  },
+  data() {
+    return {
+      images: [
+        "https://ucarecdn.com/4ee881d2-4b42-4979-b378-a46f3e185b2f/-/crop/698x524/201,0/-/preview/",
+        "https://ucarecdn.com/4ee881d2-4b42-4979-b378-a46f3e185b2f/-/crop/698x524/201,0/-/preview/",
+        "https://ucarecdn.com/4ee881d2-4b42-4979-b378-a46f3e185b2f/-/crop/698x524/201,0/-/preview/",
+      ],
+    };
+  },
+};
+</script>
+<style>
+.NuxtLogo {
+  animation: 1s appear;
+  margin: auto;
+}
+
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+}
+</style>
